cac9872cce938f8b96725030933dde69cc9483d9
李杰陽 <jay850523@icloud.com>
2020-07-17 17:14:32 +0800

    vue.config錯誤修正＝＝

--- vue.config.js
+++ vue.config.js
@@ -6,10 +6,7 @@ function resolve (dir) {
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/for-tiger/'
-    : './'
-}
-
-module.exports = {
+    : '/',
   lintOnSave: true,
   devServer: {
     disableHostCheck: true
